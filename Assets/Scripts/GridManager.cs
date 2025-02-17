d54c67e0c25696d26058e32c5f519b5162a9bc90
KrijnS <krijnvdsteen@live.nl>
2025-02-17 16:54:54 +0100

    adjust input registering

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.Profiling;
+using System.Reflection;
 
 public class GridManager : MonoBehaviour
 {
     public GameObject buttonPrefab;
     public Transform gridParent;
-    public TextMeshProUGUI selectionText;
+    public TextMeshProUGUI selectionText; // Reference to TMP Text
+
     private char[] rows = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
     private int columns = 12;
 
@@ -23,14 +26,24 @@ public class GridManager : MonoBehaviour
             {
                 GameObject button = Instantiate(buttonPrefab, gridParent);
                 string position = $"{rows[i]}{j}";
-                button.GetComponentInChildren<Text>().text = position;
-                button.GetComponent<Button>().onClick.AddListener(() => OnCellClicked(position));
+
+                // Set button text
+                TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+                if (buttonText != null) buttonText.text = position;
+
+                // Add listener properly using a captured variable
+                Button btnComponent = button.GetComponent<Button>();
+                if (btnComponent != null)
+                {
+                    btnComponent.onClick.AddListener(() => OnCellClicked(position));
+                }
             }
         }
     }
 
     void OnCellClicked(string position)
     {
+        Debug.Log("Clicked: " + position);
         if (selectionText != null)
         {
             selectionText.text = "Selected: " + position;
