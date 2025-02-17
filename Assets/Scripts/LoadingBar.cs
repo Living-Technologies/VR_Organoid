7cf697618cc15d3f7457d7cc63da6ab1850e4ea1
KrijnS <krijnvdsteen@live.nl>
2025-02-17 13:16:24 +0100

    loading bar test

---
+++ Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour
+{
+    public RectTransform _mainIcon;
+    public float _timeStep;
+    public float _oneStepAngle;
+
+    float _startTime;
+
+    void Start()
+    {
+        _startTime = Time.time;	
+    }
+    
+    void Update()
+    {
+        if(Time.time - _startTime >= _timeStep)
+        {
+            Vector3 iconAngle = _mainIcon.localEulerAngles;
+            iconAngle.z += _oneStepAngle;
+
+            _mainIcon.localEulerAngles = iconAngle;
+            
+            _startTime = Time.time;
+        }
+    }
+}
